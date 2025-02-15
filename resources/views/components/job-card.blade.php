e9a465400ef01f241b59b10a2c47ebe6e33159f7
Sushobhon Sahoo <sushobhons@gmail.com>
2025-02-15 21:11:51 +0530

    add migrations, models, controllers

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -6,9 +6,9 @@
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
+            <x-tag size="small">Backend</x-tag>
+            <x-tag size="small">Frontend</x-tag>
+            <x-tag size="small">Manager</x-tag>
         </div>
         <x-employer-logo :width="42"/>
     </div>
