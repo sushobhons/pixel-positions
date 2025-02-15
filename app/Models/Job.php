da66910111043ffb0cde65e999fb7b81a195969a
Sushobhon Sahoo <sushobhons@gmail.com>
2025-02-16 00:28:06 +0530

    add TDD Test Driven Development

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -11,6 +11,17 @@ class Job extends Model
     /** @use HasFactory<\Database\Factories\JobFactory> */
     use HasFactory;
 
+    public function tag(string $name): void
+    {
+        $tag = Tag::firstOrCreate(['name' => $name]);
+        $this->tags()->attach($tag);
+    }
+
+    public function tags(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
     public function employer(): BelongsTo
     {
         return $this->belongsTo(Employer::class);
